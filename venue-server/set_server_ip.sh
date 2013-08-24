a7bc95d7fc9ef2656e7a463fa25b5c43f92f638d
Devon <devon@indirectlylit.com>
2013-08-24 18:53:48 -0400

    wifi / ethernet options

--- venue-server/set_server_ip.sh
+++ venue-server/set_server_ip.sh
@@ -3,5 +3,10 @@
 echo "Setting up a new static IP."
 echo "Your SSH connection may hang - if so, kill it and log back in."
 
-sudo /sbin/ifconfig eth0 10.0.0.10 netmask 255.255.255.0 up
-sudo /sbin/route add default gw 10.0.0.1 eth0
+# ethernet
+# sudo /sbin/ifconfig eth0 10.0.0.10 netmask 255.255.255.0 up
+# sudo /sbin/route add default gw 10.0.0.1 eth0
+
+# wifi
+sudo /sbin/ifconfig eth1 10.0.0.10 netmask 255.255.255.0 up
+sudo /sbin/route add default gw 10.0.0.1 eth1
