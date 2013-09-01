d019cd720e9f62a6e074fe174845e890653e68cd
Jonathan Dahan <jonathan@jedahan.com>
2013-09-01 18:46:21 -0400

    update initscripts

--- venue-server/raspberrypi/venue-server.sh
+++ venue-server/raspberrypi/venue-server.sh
@@ -6,7 +6,7 @@
 case "$1" in
   start)
     echo "Starting venue-server"
-    node /home/pi/venue-server/venue-server/src/app
+    /opt/node/bin/node /home/pi/venue-server/venue-server/src/app
     ;;
   *)
     echo "Usage: /etc/init.d/venue-server {start}"
