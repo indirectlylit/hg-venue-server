9cd8e7097e36da10cacb1481a4adb22013e156e6
indirectlylit <devon@indirectlylit.com>
2014-02-26 18:43:18 -0600

    refactoring sensors module

--- src/app.sensors.js
+++ src/app.sensors.js
@@ -9,6 +9,7 @@
 //// EXTERNAL MODULES
 
 var _ = require('lodash');
+var events = require('events');
 
 
 //// INTERNAL MODULES
@@ -21,10 +22,15 @@ var app_web = require("./app.web");
 
 //// LOCAL VARIABLES
 
-var dataBuffer = {};
+var eventEmitter = new events.EventEmitter();
+var dataBuffers = {};
+var recentStats = {};
+var windowPeriod = app_settings.get('client_update_period');
 
 
-function handleIncomingData(message, address) {
+//// LOCAL FUNCTIONS
+
+var handleIncomingData = function(message, address) {
   var isodate = new Date().toISOString();
   var data = {};
 
@@ -51,15 +57,52 @@ function handleIncomingData(message, address) {
     data['size'] = message.length;
   }
 
-  if (!dataBuffer[address]) {
-    dataBuffer[address] = [];
+  if (!dataBuffers[address]) {
+    dataBuffers[address] = [];
   }
 
-  dataBuffer[address].push(data);
+  dataBuffers[address].push(data);
 
-  app_logger.write(data);
-}
+  eventEmitter.emit('data', data);
+};
 
+
+var genStats = function(data) {
+  // find message rate
+  var stats = {};
+  stats['message_rate'] = 1000*1.0*data.length/windowPeriod;
+
+
+  // find average message size and max data rate
+  var minInterval = 0;
+
+  totalBytes = 0;
+  _.forEach(data, function(message, index) {
+    totalBytes += message['size'];
+    minInterval += message['data']['interval'];
+  });
+
+  minInterval /= data.length; // average
+  // prevent divide-by-zero issue
+  minInterval = minInterval === 0 ? 1e-9 : minInterval;
+  stats['max_rate'] = 1e6 / minInterval; // interval in microseconds to Hz
+  stats['data_rate'] = 1000*1.0*totalBytes/windowPeriod;
+  stats['avg_size'] = totalBytes/data.length;
+
+
+  // find dropped messages
+  var dropped = 0;
+  var counterList = _.pluck(_.pluck(data, 'data'), 'counter');
+
+  counterList.sort();
+  for (i = 1; i < counterList.length; i++) {
+      if (counterList[i]-1 != counterList[i-1]) {
+        dropped += counterList[i] - counterList[i-1];
+      }
+  }
+  stats['drop_rate'] = 1000*(dropped/windowPeriod);
+  return stats;
+};
 
 
 app_udpServer.on("message", function (msg, rinfo) {
@@ -75,52 +118,13 @@ app_serialServer.on("data", function(data) {
 
 // sensor stats
 setInterval(function() {
-  var windowOfStats = {};
-  _.forEach(dataBuffer, function(data, key) {
-
-
-    // find message rate
-    var stats = {};
-    stats['message_rate'] = 1000*1.0*data.length/app_settings.get('client_update_period');
-
-
-    // find average message size and max data rate
-    var minInterval = 0;
-
-    totalBytes = 0;
-    _.forEach(data, function(message, index) {
-      totalBytes += message['size'];
-      minInterval += message['data']['interval'];
-    });
-
-    minInterval /= data.length; // average
-    // prevent divide-by-zero issue
-    minInterval = minInterval === 0 ? 1e-9 : minInterval;
-    stats['max_rate'] = 1e6 / minInterval; // interval in microseconds to Hz
-    stats['data_rate'] = 1000*1.0*totalBytes/app_settings.get('client_update_period');
-    stats['avg_size'] = totalBytes/data.length;
-
-
-    // find dropped messages
-    var dropped = 0;
-    var counterList = _.pluck(_.pluck(data, 'data'), 'counter');
-
-    counterList.sort();
-    for (i = 1; i < counterList.length; i++) {
-        if (counterList[i]-1 != counterList[i-1]) {
-          dropped += counterList[i] - counterList[i-1];
-        }
-    }
-    stats['drop_rate'] = 1000*(dropped/app_settings.get('client_update_period'));
-
-    windowOfStats[key] = stats;
-  });
-  app_web.writeToWebSockets('sensorStats', windowOfStats);
-  dataBuffer = {};
-}, app_settings.get('client_update_period'));
+  recentStats = _.map(dataBuffers, genStats);
+  eventEmitter.emit('stats', recentStats);
+  dataBuffers = {};
+}, windowPeriod);
 
 
 
 //// EXPORTS
 
-
+module.exports = eventEmitter;
