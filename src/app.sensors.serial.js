379ef5327aa0597f1b255e5454f16f2ade04a645
indirectlylit <devon@indirectlylit.com>
2014-04-08 00:58:48 -0400

    moving to hopefully cleaner logic on serial data handling

--- src/app.sensors.serial.js
+++ src/app.sensors.serial.js
@@ -8,18 +8,15 @@
  */
 
 
+//// EXTERNAL MODULES
+
 var _ = require('lodash');
 var events = require('events');
+var os = require('os');
 var serialport = require("serialport");
 
-var SERIAL_PORTS = ["ttyAMA0", "ttyUSB0"];
-var SERIAL_RATE = 57600;
 
-// this logic may not work if a machine uses multiple ports
-var serial_active = false;
-
-
-var eventEmitter = new events.EventEmitter();
+//// LOCAL FUNCTIONS
 
 function createSerialPort(name) {
   var port = new serialport.SerialPort("/dev/" + name,
@@ -36,29 +33,36 @@ function createSerialPort(name) {
   return port;
 }
 
-var serialPorts = _.map(SERIAL_PORTS, createSerialPort);
-
 function attemptLogging() {
-  _.each(serialPorts, function(port){
-    if (!serial_active) {
-      port.open(function(error) {
-        if (error) {
-          serial_active = false;
-          return;
-        }
-        serial_active = true;
-        console.log("\n----\nOpened SerialPort at "+Date.now()+"\n----\n");
-      });
-    }
-  });
+  if (!serial_active) {
+    serialPort.open(function(error) {
+      if (error) {
+        serial_active = false;
+        return;
+      }
+      serial_active = true;
+      console.log("\n----\nOpened SerialPort at "+Date.now()+"\n----\n");
+    });
+  }
 }
 
-setInterval(function() {
-  if (!serial_active) {
-    attemptLogging();
-  }
-}, 1000);
+//// LOCAL VARIABLES
 
+var SERIAL_RATE = 57600;
+
+var serial_active = false;
+var eventEmitter = new events.EventEmitter();
+
+var portName = os.arch() === "arm" ? "ttyAMA0" : "ttyUSB0";
+var serialPort = createSerialPort(portName);
+
+
+//// MODULE LOGIC
+
+setInterval(attemptLogging, 1000);
+
+
+//// EXPORTS
 
 // emits 'message' events
 module.exports = eventEmitter;
