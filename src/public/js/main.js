666842c9b4418f2bbea7909a0ff2f402547ac931
indirectlylit <devon@indirectlylit.com>
2014-05-15 15:23:51 -0500

    migrate network stats to its own view

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -18,10 +18,10 @@ $(function() {
     connectionState   : $('.js-connectionState'),
     serverStats       : $('.js-serverStats'),
     settings          : $('.js-settings'),
-    sensorStats       : $('.js-sensorStats')
   };
 
   app.views = app.views || {};
+  app.views.network         = new app.views.Network().render();
   app.views.serverSettings  = new app.views.ServerSettings().render();
   app.views.logList         = new app.views.LogList().render();
   app.views.recorder        = new app.views.Recorder().render();
@@ -34,7 +34,6 @@ $(function() {
   // $.pnotify.defaults.history = false;
 
   // pre-render
-  app.dom.sensorStats.html(app.utils.render('sensorStats'));
   app.dom.serverStats.html(app.utils.render('serverStats'));
 
   // hide settings when not in advanced mode
@@ -44,6 +43,7 @@ $(function() {
   app.data = app.data || {};
   app.data.clientAddresses = [];
   app.data.fileName = "";
+  app.data.networkStats = {};
 
 
   /*********************/
@@ -51,40 +51,8 @@ $(function() {
   /*********************/
   app.websocket.on('sensors.stats', function(newStats) {
     app.data.clientAddresses = _.union(app.data.clientAddresses, _.keys(newStats)).sort();
-
-    var tableRows = _.map(app.data.clientAddresses, function(address) {
-      var stats = newStats[address];
-
-      row = {};
-      row.address = address === "serial port" ? "Controller" : address;
-
-      if (stats) { // we have new data
-        row.message_rate =  _.has(stats, 'message_rate') ?
-                                  stats.message_rate.toFixed(1) : '?';
-        row.drop_rate =     _.has(stats, 'drop_rate') ?
-                                  stats.drop_rate.toFixed(1) : '?';
-        row.data_rate =     _.has(stats, 'data_rate') ?
-                                  (stats.data_rate/1024).toFixed(2) : '?';
-        row.voltage =       _.has(stats, 'avg_v') ?
-                                  (stats.avg_v).toFixed(2) : '?';
-        row.power_in =      _.has(stats, 'avg_c_in') ?
-                                  (stats.avg_v * stats.avg_c_in).toFixed(2) : '?';
-        row.power_out =     _.has(stats, 'avg_c_out') ?
-                                  (stats.avg_v * stats.avg_c_out).toFixed(2) : '?';
-        row.connected =     true;
-      }
-      else { // no data received from this address
-        row.message_rate = 0;
-        row.drop_rate =    "";
-        row.data_rate =    (0).toFixed(2);
-        row.connected =    false;
-        row.voltage =      "";
-        row.power_in =     "";
-        row.power_out =    "";
-      }
-      return row;
-    });
-    app.dom.sensorStats.html(app.utils.render('sensorStats', {tableRows: tableRows}));
+    app.data.networkStats = newStats;
+    app.views.network.render();
   });
 
 
---
+++ src/public/js/views/Network.js
@@ -0,0 +1,59 @@
+
+// Devon Rueckner
+// The Human Grid
+// All Rights Reserved
+
+var app = app || {};
+app.views = app.views || {};
+
+
+app.views.Network = Backbone.Viewmaster.extend({
+  el: $('.js-network'),
+  template: function(context) {
+    return app.utils.render('sensorStats', context);
+  },
+  context: function() {
+    var tableRows = _.map(app.data.clientAddresses, function genStatsTableRow(address) {
+      var stats = app.data.networkStats[address];
+
+      row = {};
+      row.address = address === "serial port" ? "Controller" : address;
+
+      if (stats) { // we have new data
+        row.message_rate =  _.has(stats, 'message_rate') ?
+                                  stats.message_rate.toFixed(1) : '?';
+        row.drop_rate =     _.has(stats, 'drop_rate') ?
+                                  stats.drop_rate.toFixed(1) : '?';
+        row.data_rate =     _.has(stats, 'data_rate') ?
+                                  (stats.data_rate/1024).toFixed(2) : '?';
+        row.voltage =       _.has(stats, 'avg_v') ?
+                                  (stats.avg_v).toFixed(2) : '?';
+        row.power_in =      _.has(stats, 'avg_c_in') ?
+                                  (stats.avg_v * stats.avg_c_in).toFixed(2) : '?';
+        row.power_out =     _.has(stats, 'avg_c_out') ?
+                                  (stats.avg_v * stats.avg_c_out).toFixed(2) : '?';
+        row.connected =     true;
+      }
+      else { // no data received from this address
+        row.message_rate = 0;
+        row.drop_rate =    "";
+        row.data_rate =    (0).toFixed(2);
+        row.connected =    false;
+        row.voltage =      "";
+        row.power_in =     "";
+        row.power_out =    "";
+      }
+      return row;
+    });
+    return {
+      'tableRows': tableRows
+    };
+  },
+  initialize: function() {
+  },
+  events: function() {
+    return {};
+  }
+});
+
+
