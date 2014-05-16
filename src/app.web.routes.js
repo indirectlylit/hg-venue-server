5e9c7333256a4c0ed52a408548137aefb3bdf431
indirectlylit <devon@indirectlylit.com>
2014-05-16 12:49:50 -0500

    moving all logger routes to their own file

--- src/app.web.routes.js
+++ src/app.web.routes.js
@@ -10,9 +10,10 @@ var path = require('path');
 
 
 var app_gpio = require("./app.gpio");
-var app_serverStats = require("./app.serverStats");
 var app_logger = require("./app.logger");
+var app_serverStats = require("./app.serverStats");
 var app_web = require("./app.web");
+var app_web_routes_logger = require("./app.web.routes.logger");
 
 
 app_web.route('get', '/', function(req, res) {
@@ -48,6 +49,7 @@ app_web.route('get', '/', function(req, res) {
   });
 });
 
+
 app_web.route('put', '/api/squarewave/', function(req, res) {
   var state = req.body;
   if (!_.contains([true, false], state)) {
@@ -64,86 +66,6 @@ app_web.route('put', '/api/squarewave/', function(req, res) {
   });
 });
 
-app_web.route('put', '/api/logger/start', function(req, res) {
-  app_logger.startLogging(function(err, state){
-    if (err) {
-      console.log("Error:", err);
-      return res.json(500, err);
-    }
-    res.json(state);
-  });
-});
-
-app_web.route('put', '/api/logger/stop', function(req, res) {
-  app_logger.stopLogging(function(err, state){
-    if (err) {
-      console.log("Error:", err);
-      return res.json(500, err);
-    }
-    res.json(state);
-  });
-});
-
-app_web.route('put', '/api/logger/reset', function(req, res) {
-  app_logger.reset(function(err, state){
-    if (err) {
-      console.log("Error:", err);
-      return res.json(500, err);
-    }
-    res.json(state);
-  });
-});
-
-app_web.route('post', '/api/logger/save_as', function(req, res) {
-  app_logger.saveAs(req.body, function(err){
-    if (err) {
-      console.log("Error:", err);
-      return res.json(500, err);
-    }
-    app_logger.getAllFileInfo(function(err, file_info) {
-      res.json(file_info);
-    });
-  });
-});
-
-app_web.route('delete', '/api/logger/files/:id', function(req, res) {
-  app_logger.deleteFile(req.params.id, function(err){
-    if (err) {
-      console.log("Error:", err);
-      return res.json(500, err);
-    }
-    app_logger.getAllFileInfo(function(err, file_info) {
-      res.json(file_info);
-    });
-  });
-});
-
-app_web.route('get', '/api/logger/files/:id', function(req, res) {
-  app_logger.getFileInfo(req.params.id, function (err, fileInfo){
-    if (err) {
-      console.log("Error:", err);
-      return res.json(500, err);
-    }
-    res.download(path.join(app_logger.dataDir(), fileInfo.fName));
-  });
-});
-
-app_web.route('put', '/api/logger/external', function(req, res) {
-  if (!_.contains([true, false], req.body)) {
-    var err = "'on' must be true or false";
-    console.log("Error:", err);
-    return res.json(400, err);
-  }
-  app_logger.setExternal(req.body, function(err, state){
-    if (err) {
-      console.log("Error:", err);
-      return res.json(400, err);
-    }
-    app_logger.getAllFileInfo(function(err, file_info) {
-      res.json(file_info);
-    });
-  });
-});
 
 app_web.route('get', '/api/socket_ready', function(req, res) {
   res.json(app_web.socketReady());
---
+++ src/app.web.routes.logger.js
@@ -0,0 +1,97 @@
+
+
+// Devon Rueckner
+// The Human Grid
+// All Rights Reserved
+
+
+var _ = require('lodash');
+var path = require('path');
+
+
+var app_logger = require("./app.logger");
+var app_web = require("./app.web");
+
+
+
+app_web.route('put', '/api/logger/start', function(req, res) {
+  app_logger.startLogging(function(err, state){
+    if (err) {
+      console.log("Error:", err);
+      return res.json(500, err);
+    }
+    res.json(state);
+  });
+});
+
+app_web.route('put', '/api/logger/stop', function(req, res) {
+  app_logger.stopLogging(function(err, state){
+    if (err) {
+      console.log("Error:", err);
+      return res.json(500, err);
+    }
+    res.json(state);
+  });
+});
+
+app_web.route('put', '/api/logger/reset', function(req, res) {
+  app_logger.reset(function(err, state){
+    if (err) {
+      console.log("Error:", err);
+      return res.json(500, err);
+    }
+    res.json(state);
+  });
+});
+
+app_web.route('post', '/api/logger/save_as', function(req, res) {
+  app_logger.saveAs(req.body, function(err){
+    if (err) {
+      console.log("Error:", err);
+      return res.json(500, err);
+    }
+    app_logger.getAllFileInfo(function(err, file_info) {
+      res.json(file_info);
+    });
+  });
+});
+
+app_web.route('delete', '/api/logger/files/:id', function(req, res) {
+  app_logger.deleteFile(req.params.id, function(err){
+    if (err) {
+      console.log("Error:", err);
+      return res.json(500, err);
+    }
+    app_logger.getAllFileInfo(function(err, file_info) {
+      res.json(file_info);
+    });
+  });
+});
+
+app_web.route('get', '/api/logger/files/:id', function(req, res) {
+  app_logger.getFileInfo(req.params.id, function (err, fileInfo){
+    if (err) {
+      console.log("Error:", err);
+      return res.json(500, err);
+    }
+    res.download(path.join(app_logger.dataDir(), fileInfo.fName));
+  });
+});
+
+app_web.route('put', '/api/logger/external', function(req, res) {
+  if (!_.contains([true, false], req.body)) {
+    var err = "'on' must be true or false";
+    console.log("Error:", err);
+    return res.json(400, err);
+  }
+  app_logger.setExternal(req.body, function(err, state){
+    if (err) {
+      console.log("Error:", err);
+      return res.json(400, err);
+    }
+    app_logger.getAllFileInfo(function(err, file_info) {
+      res.json(file_info);
+    });
+  });
+});
+
