ce1c7b39bff07469d2f3704a785eacdbdc2051eb
indirectlylit <devon@indirectlylit.com>
2015-02-18 14:37:23 -0500

    add new machine kind

--- src/public/js/views/ACSensors.js
+++ src/public/js/views/ACSensors.js
@@ -14,7 +14,7 @@ app.views.ACSensors = Backbone.Viewmaster.extend({
   context: function() {
     var acsensorStats = _.sortBy(
       _.where(app.state.networkStats, function findAC(statsObj) {
-        return statsObj.last_msg.kind === '3-ac';
+        return statsObj.last_msg.kind === '3-ac' || statsObj.last_msg.kind === 'proto-em';
       }),
       function (statObj){
         return statObj.last_msg.uid;
