41346f412bab4b90cb76eaab5da59ef6a431c5c0
indirectlylit <devon@indirectlylit.com>
2015-04-15 19:17:19 -0500

    use ethernet instead of wifi when running in a VM

--- set_server_ip.sh
+++ set_server_ip.sh
@@ -4,9 +4,9 @@ echo "Setting up a new static IP."
 echo "Your SSH connection may hang - if so, kill it and log back in."
 
 # ethernet
-# sudo /sbin/ifconfig eth0 10.0.0.10 netmask 255.255.255.0 up
-# sudo /sbin/route add default gw 10.0.0.1 eth0
+sudo /sbin/ifconfig eth0 10.0.0.10 netmask 255.255.255.0 up
+sudo /sbin/route add default gw 10.0.0.1 eth0
 
 # wifi
-sudo /sbin/ifconfig eth1 10.0.0.10 netmask 255.255.255.0 up
-sudo /sbin/route add default gw 10.0.0.1 eth1
+# sudo /sbin/ifconfig eth1 10.0.0.10 netmask 255.255.255.0 up
+# sudo /sbin/route add default gw 10.0.0.1 eth1
