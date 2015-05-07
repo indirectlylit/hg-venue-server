7e95c81d7ad1535a461bb750f87a3b970e993293
indirectlylit <devon@indirectlylit.com>
2015-05-07 12:17:06 -0400

    update to use chaining

--- src/public/js/views/Bikes.js
+++ src/public/js/views/Bikes.js
@@ -12,27 +12,28 @@ app.views.Bikes = Backbone.Viewmaster.extend({
     return app.utils.render('bikes', context);
   },
   context: function() {
-    var bikeStats = _.where(app.state.networkStats, {'kind': app.KIND.BIKE})
-    var rows = _.map(bikeStats, function(stats, i){
-      var power_out = stats.avg_v * stats.avg_c_out[0];
-      var row = {
-        power_out: power_out.toFixed(0),
-        power_out_pct: 100.0 * (power_out / app.maxGraph),
-      };
-      if (app.state.labels.bikes[stats.uid]) {
-        row.unlabeled = false;
-        row.label = app.state.labels.bikes[stats.uid];
-      }
-      else {
-        row.unlabeled = true;
-        row.label = '# '+stats.uid;
-      }
-      return row;
-    });
+    var rows = _(app.state.networkStats)
+      .where({'kind': app.KIND.BIKE})
+      .map(function(stats, i){
+        var power_out = stats.avg_v * stats.avg_c_out[0];
+        var row = {
+          power_out: power_out.toFixed(0),
+          power_out_pct: 100.0 * (power_out / app.maxGraph),
+        };
+        if (app.state.labels.bikes[stats.uid]) {
+          row.unlabeled = false;
+          row.label = app.state.labels.bikes[stats.uid];
+        }
+        else {
+          row.unlabeled = true;
+          row.label = '# '+stats.uid;
+        }
+        return row;
+      })
+      .sortByOrder(['unlabeled', 'label'])
+      .value();
 
-    return {
-      'rows': _.sortByOrder(rows, ['unlabeled', 'label'])
-    };
+    return { 'rows': rows };
   },
   initialize: function() {
   },
